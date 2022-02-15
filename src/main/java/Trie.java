b58215c22f1b14c6994f319aa7892f189749b4ce
noharms <47367811+noharms@users.noreply.github.com>
2022-02-15 21:19:23 +0100

    Fix bugs in trie; Add tests for trie

--- src/main/java/Trie.java
+++ src/main/java/Trie.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * A prefix search tree based on the lower case latin alphabet 'a-z'
- * (upper case and non-alphabetic letters are ignored).
+ * A prefix search tree based on the lower case latin alphabet 'a-z' - upper case letters are converted to lower case
+ * and non-alphabetic letters are ignored.
  *
  * <ul>
  *     <li>{@code void add(String validWord} in O(1)</li>
@@ -15,27 +16,27 @@ public class Trie {
 
     private static final int ALPHABET_SIZE = 26; // a-z
 
-    private static class TreeNode {
+    private static class TrieNode {
 
-        private TreeNode[] children = new TreeNode[ALPHABET_SIZE];
+        private TrieNode[] children = new TrieNode[ALPHABET_SIZE];
 
         private boolean isValidWord;
 
     }
 
-    private final TreeNode root;
+    private final TrieNode root;
 
     public Trie() {
-        root = new TreeNode();
+        root = new TrieNode();
     }
 
     public void add(String validWord) {
-        TreeNode current = root;
+        TrieNode current = root;
         for (char c : validWord.toLowerCase().toCharArray()) {
             if (c >= 'a' && c <= 'z') {
                 int index = c - 'a';
                 if (current.children[index] == null) {
-                    current.children[index] = new TreeNode();
+                    current.children[index] = new TrieNode();
                 }
                 current = current.children[index];
             }
@@ -44,7 +45,7 @@ public class Trie {
     }
 
     public boolean isValid(String word) {
-        TreeNode current = root;
+        TrieNode current = root;
         for (char c : word.toLowerCase().toCharArray()) {
             if (c >= 'a' && c <= 'z') {
                 int index = c - 'a';
@@ -59,23 +60,36 @@ public class Trie {
     }
 
     public List<String> autoComplete(String prefix) {
-        TreeNode current = root;
+        TrieNode current = root;
+        StringBuilder cleanedPrefixBuilder = new StringBuilder();
         for (char c : prefix.toLowerCase().toCharArray()) {
             if (c >= 'a' && c <= 'z') {
                 int index = c - 'a';
                 if (current.children[index] == null) {
-                    return null;
+                    current = null;
+                    break;
                 } else {
+                    cleanedPrefixBuilder.append(c);
                     current = current.children[index];
                 }
             }
         }
-        List<String> valids = new ArrayList<>();
-        findValidDescendants(current, new StringBuilder(prefix), valids);
-        return valids;
+        TrieNode terminalNode = current;
+        String cleanedPrefix = cleanedPrefixBuilder.toString();
+        return findValidDescendants(terminalNode).stream().map(cleanedPrefix::concat).toList();
     }
 
-    private void findValidDescendants(TreeNode current, StringBuilder candidate, List<String> valids) {
+    private List<String> findValidDescendants(TrieNode node) {
+        if (node == null) {
+            return Collections.emptyList();
+        } else {
+            List<String> valids = new ArrayList<>();
+            findValidDescendants(node, new StringBuilder(), valids);
+            return valids;
+        }
+    }
+
+    private void findValidDescendants(TrieNode current, StringBuilder candidate, List<String> valids) {
         if (current == null) {
             return;
         } else if (current.isValidWord) {
@@ -83,7 +97,7 @@ public class Trie {
         }
         for (int i = 0; i < ALPHABET_SIZE; ++i) {
             char c = (char) ('a' + i);
-            TreeNode child = current.children[i];
+            TrieNode child = current.children[i];
             findValidDescendants(child, candidate.append(c), valids);
             candidate.deleteCharAt(candidate.length() - 1);
         }
--- src/test/java/TrieTest.java
+++ src/test/java/TrieTest.java
@@ -1,13 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrieTest {
 
     @Test
-    void emptyTrie_search_gives_false() {
+    void emptyTrie_isValid_givesFalse() {
         Trie trie = new Trie();
-        assertFalse(trie.isValid(""));
         assertFalse(trie.isValid("a"));
         assertFalse(trie.isValid("A"));
         assertFalse(trie.isValid("$"));
@@ -15,4 +17,87 @@ class TrieTest {
         assertFalse(trie.isValid("abcdef"));
     }
 
+    @Test
+    void emptyTree_autoComplete_givesEmptyList() {
+        Trie trie = new Trie();
+        assertEquals(emptyList(), trie.autoComplete(""));
+        assertEquals(emptyList(), trie.autoComplete("a"));
+        assertEquals(emptyList(), trie.autoComplete("A"));
+        assertEquals(emptyList(), trie.autoComplete("$"));
+        assertEquals(emptyList(), trie.autoComplete(" "));
+        assertEquals(emptyList(), trie.autoComplete("abcdef"));
+    }
+
+    @Test
+    void isValid_emptyPrefixIsAlwaysFalse() {
+        Trie trie = new Trie();
+        trie.add("Hello");
+        trie.add("world");
+        assertFalse(trie.isValid(""));
+        assertFalse(new Trie().isValid(""));
+    }
+
+    @Test
+    void isValid_isCaseInsensitive() {
+        Trie trie = new Trie();
+        trie.add("Hello");
+        trie.add("world");
+        assertTrue(trie.isValid("Hello"));
+        assertTrue(trie.isValid("HELLO"));
+        assertTrue(trie.isValid("HeLlO"));
+        assertTrue(trie.isValid("hello"));
+        assertTrue(trie.isValid("%§??!#<^^hello%§??!#<^^"));
+        assertTrue(trie.isValid("he%§ll??!#<^^o"));
+        assertTrue(trie.isValid("World"));
+        assertTrue(trie.isValid("WORLD"));
+        assertTrue(trie.isValid("wOrLd"));
+        assertTrue(trie.isValid("world"));
+        assertFalse(trie.isValid("other"));
+    }
+
+    @Test
+    void isValid_ignoresNonAlphabetic() {
+        Trie trie = new Trie();
+        trie.add("hello");
+        assertTrue(trie.isValid("0h1e2l3l4o5"));
+        assertTrue(trie.isValid("%§??!#<^^hello%§??!#<^^"));
+        assertTrue(trie.isValid("he%§ll??!#<^^o"));
+    }
+
+    @Test
+    void isValid_emptyPrefixGivesAllValidWords() {
+        Trie trie = new Trie();
+        trie.add("Hello");
+        trie.add("world");
+        trie.add("how");
+        trie.add("are");
+        trie.add("u?");
+
+        assertEquals(List.of("are", "hello", "how", "u", "world"), trie.autoComplete(""));
+        assertEquals(List.of("are", "hello", "how", "u", "world"), trie.autoComplete(" "));
+        assertEquals(List.of("are", "hello", "how", "u", "world"), trie.autoComplete("123!?="));
+    }
+
+
+    @Test
+    void autoComplete_works() {
+        Trie trie = new Trie();
+        trie.add("slow");
+        trie.add("song");
+        trie.add("stock");
+        trie.add("steam");
+        trie.add("star");
+        trie.add("string");
+        trie.add("strap");
+        trie.add("stroll");
+        trie.add("stream");
+        trie.add("others");
+
+        assertEquals(List.of("slow", "song", "star", "steam", "stock", "strap", "stream", "string", "stroll"),
+                     trie.autoComplete("s"));
+        assertEquals(List.of("slow"), trie.autoComplete("sl"));
+        assertEquals(List.of("star", "steam", "stock", "strap", "stream", "string", "stroll"),
+                     trie.autoComplete("st"));
+        assertEquals(List.of("strap", "stream", "string", "stroll"), trie.autoComplete("str"));
+    }
 }
