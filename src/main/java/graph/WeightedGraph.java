dcb290aee42271f84a90d61efd35298b293fab74
noharms <harms.no@gmail.com>
2022-10-30 10:05:16 +0100

    Add optimised djikstra algorithm using min priority queue (and simplifying old version a bit)

--- src/main/java/graph/WeightedGraph.java
+++ src/main/java/graph/WeightedGraph.java
@@ -70,17 +70,25 @@ public class WeightedGraph<T> extends Graph<T> {
         return dijkstra(from, to);
     }
 
+    /**
+     * A bit simpler implementation than the original algorithm by Djikstra:
+     * instead of using a min priority queue to find the next cheapest
+     * unvisited node to consider, we simply search the whole graph each time again. As a consequence, this algorithm
+     * is O(n^2) instead of O((n+m)*log(n)), where n is the number of nodes and m the number of edges.
+     */
+    // note: could be generalised to return the distances to all nodes by not stopping once a target is found
     private List<T> dijkstra(T from, T to) {
         Set<T> visited = new HashSet<>();
-        Map<T, Integer> nodeToDistance = initializeDistances(from);
+        Map<T, Integer> nodeToDistance = initialNodeToDistanceMap(from);
         Map<T, T> nodeToParent = new HashMap<>();
 
         Optional<T> nextNode = Optional.of(from);
         while (nextNode.isPresent() && !nextNode.get().equals(to)) {
             T current = nextNode.get();
             for (T neighbor : unvisitedNeighbors(current, visited)) {
+                int oldDistanceToNeighbor = nodeToDistance.get(neighbor);
                 int newDistanceToNeighbor = nodeToDistance.get(current) + weight(current, neighbor);
-                if (newDistanceToNeighbor < nodeToDistance.get(neighbor)) {
+                if (newDistanceToNeighbor < oldDistanceToNeighbor) {
                     nodeToDistance.put(neighbor, newDistanceToNeighbor);
                     nodeToParent.put(neighbor, current);
                 }
@@ -91,14 +99,13 @@ public class WeightedGraph<T> extends Graph<T> {
         return nextNode.isPresent() ? reconstructPath(to, nodeToParent) : new LinkedList<>();
     }
 
-    private Map<T, Integer> initializeDistances(T startNode) {
-        Set<T> directNeighbors = allNeighbors(startNode);
+    private Map<T, Integer> initialNodeToDistanceMap(T startNode) {
         return nodesToEdges.keySet().stream()
                            .collect(Collectors.toMap(
-                               node -> node,
-                               node -> directNeighbors.contains(node) ? weight(startNode, node) : INFINITY,
-                               (key, val) -> val,
-                               HashMap::new
+                                   node -> node,
+                                   node -> node.equals(startNode) ? 0 : INFINITY,
+                                   (key, val) -> val,
+                                   HashMap::new
                            ));
     }
 
@@ -109,5 +116,45 @@ public class WeightedGraph<T> extends Graph<T> {
                            .min(Comparator.comparingInt(nodeToMinimalDistance::get));
     }
 
+    /**
+     * Differs from {@link WeightedGraph#shortestPath(Object, Object)} in that it uses the original algorithm
+     * with a min priority queue as an optimisation. This should be O((n+m) * log(n)), n number nodes, m number edges
+     */
+    public List<T> shortestPathUsingMinQueue(T from, T to) {
+        throwIfNotFound(from);
+        throwIfNotFound(to);
+        return dijkstraUsingMinQueue(from, to);
+    }
+
+    private List<T> dijkstraUsingMinQueue(T from, T to) {
+        Set<T> visited = new HashSet<>();
+        Map<T, Integer> nodeToDistance = initialNodeToDistanceMap(from);
+        Map<T, T> nodeToParent = new HashMap<>();
+        PriorityQueue<NodeWithPriority<T>> searchQueue = new PriorityQueue<>();
+        searchQueue.add(new NodeWithPriority<>(from, 0));
+
+        while (!searchQueue.isEmpty() && !searchQueue.peek().node().equals(to)) {
+            T current = searchQueue.poll().node();
+            for (T neighbor : unvisitedNeighbors(current, visited)) {
+                int oldDistanceToNeighbor = nodeToDistance.get(neighbor);
+                int newDistanceToNeighbor = nodeToDistance.get(current) + weight(current, neighbor);
+                if (newDistanceToNeighbor < oldDistanceToNeighbor) {
+                    nodeToDistance.put(neighbor, newDistanceToNeighbor);
+                    nodeToParent.put(neighbor, current);
+                }
+                searchQueue.remove(new NodeWithPriority<>(neighbor, oldDistanceToNeighbor)); // O(log(n))
+                searchQueue.add(new NodeWithPriority<>(neighbor, newDistanceToNeighbor)); // O(log(n))
+            }
+            visited.add(current);
+        }
+        return searchQueue.isEmpty() ? new LinkedList<>() : reconstructPath(to, nodeToParent);
+    }
+
+    private static record NodeWithPriority<V>(V node, int priority) implements Comparable<NodeWithPriority<V>> {
+        @Override
+        public int compareTo(WeightedGraph.NodeWithPriority<V> o) {
+            return priority - o.priority;
+        }
+    }
 }
 
